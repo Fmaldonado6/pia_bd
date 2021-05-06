bca2675527e3924f566fe3fb6257ba22051a7c9b
Arturo Isac Luna Vázquez <65917254+IsacLuna@users.noreply.github.com>
2021-05-06 10:45:47 -0500

    Clases en modelos
    Agregué: TipoEmpleado, Privilegios, Alimentos, TipoAlimento, Marca y Pedido

--- src/models/models.ts
+++ src/models/models.ts
@@ -10,4 +10,47 @@ export class Empleado {
     idTipoEmpleado: string = ""
     numero: number = 0
     fechaNacimiento?: Date
+    contrasena: string = ""
+}
+
+export class TipoEmpleado {
+    idTipoEmpleado: string = ""
+    nombreTipoEmpleado: string = ""
+    sueldo: number = 0
+    horaEntrada?: Date
+    horaSalida?: Date
+}
+
+export class Privilegios {
+    idPrivilegio: string = ""
+    nombre: string = ""
+}
+
+export class Alimentos {
+    idAlimento: string = ""
+    idTipoAlimento: string = ""
+    idMarca: string = ""
+    nombre: string = ""
+    precio: number = 0
+    descripcion: string = ""
+    cantidadDisponible: number = 0
+}
+
+export class TipoAlimento {
+    tipoAlimento: string = ""
+    nombreTipo: string = ""
+}
+
+export class Marca {
+    idMarca: string = ""
+    nombreMarca: string = ""
+}
+
+export class Pedido {
+    idPedido: string = ""
+    idEmpleado: string = ""
+    fechaPedido?: Date
+    subtotal: number = 0
+    descuento: number = 0
+    total: number = 0
 }
